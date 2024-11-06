5796e8ac90e84696f53f5ce060d6f1530e6957c6
Siddharth Agarwala <siddharthagarwala1@gmail.com>
2024-11-05 18:05:18 -0600

    Added Comments and fixed Implementation
    Printer Environment works. When I run the code, I get the correct base locations and arm positions.
    
    Fixed the issue with the overlapping geometry and began adding the HTTP requests.
    
    Starting to add the workspace wrapper class and properly commenting with docstrings. Will add more Detailed comments soon.

--- Full Envelope Managment.py
+++ Full Envelope Managment.py
@@ -4,7 +4,6 @@ Created on Fri Oct 25 15:14:44 2024
 
 @author: Siddh
 """
-# import TestCode
 import math
 import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
@@ -12,10 +11,22 @@ import matplotlib.transforms as transforms
 #Polygon Plotting
 import numpy as np
 from shapely.geometry import Polygon
-from shapely.affinity import scale
+from itertools import combinations
+from shapely.ops import unary_union
+
+#HTTP requests
+import requests
 
 class printer:
-    def __init__ (self, OriginLocation, PrinterOffset, ax, PrinterDimensions = 300, PrinterName = "p", armOneLength = 217, armTwoLength = 204):
+    gcode_list = {"M291": 'M291 P"Waiting for permission to continue" S3', # pauses printer to wait to be allowed to start again
+                  "M292": 'M292',       # Unpauses M291 command
+                  "M23": 'M23',         # Selects sd card file for printing
+                  "M24": 'M24',         # Starts or Resumes Print from an sd card
+                  "M25": 'M25',         # Pause a print from an sd card - runs pause.g macro.
+                  "M226": 'M226',       # Never Used
+                  "M104": 'M104 S240',  # Sets the extruder temperature to 240C
+                  "M408": 'M408 S0'}    # Returns the status of the printer in json style. More info: https://reprap.org/wiki/G-code#M408:_Report_JSON-style_response
+    def __init__ (self, OriginLocation, PrinterOffset, IP, ax, PrinterDimensions = 300, PrinterName = "p", armOneLength = 217, armTwoLength = 204):
         """
         ALL UNITS are in: mm and Radians
         Args: 
@@ -29,6 +40,7 @@ class printer:
         self.OriginLocation = OriginLocation
         self.PrinterOffset = (PrinterOffset[0], 1 * PrinterOffset[1])
         self.PrinterDimensions = PrinterDimensions
+        self.IP = IP
         
         self.PrinterTransformation =  transforms.Affine2D().rotate_deg_around(self.OriginLocation[0], self.OriginLocation[1], self.OriginLocation[2]) + ax.transData
         self.PrinterCoordTranslation = transforms.Affine2D().translate(self.OriginLocation[0], self.OriginLocation[1]).rotate_deg_around(self.OriginLocation[0], self.OriginLocation[1], self.OriginLocation[2])+ ax.transData
@@ -43,27 +55,36 @@ class printer:
         ax.add_patch(self.getBuildSurfaceRectangle())
         
         
-        
+    """
+    Printer Initialization and Plotting/Joints
+    """
     def getBuildSurfaceRectangle(self):
-        """Returns the print envelope of the printer as a recangle object"""
+        """
+        Returns the print envelope of the printer as a recangle object
+        """
         return plt.Rectangle((self.OriginLocation[0],self.OriginLocation[1]),
                               self.PrinterDimensions, self.PrinterDimensions , 
                               linewidth=2, edgecolor='r', facecolor='none', transform=self.PrinterTransformation)
     
     def getPrinterPoint(self):
-        """Returns a tuple of the printer's base coordinates"""
+        """
+        Returns a tuple of the printer's base coordinates
+        """
         return (self.OriginLocation[0] + self.PrinterOffset[0], self.OriginLocation[1] + self.PrinterOffset[1])
     
-    def getEndPosition(self):
-        return(0,0,0)
-    
-    
     def plotPrinterPoint(self, xyz):
-        """Takes a tuple (x,y,z)"""
+        """
+        Args: Takes a tuple (x,y,z)
+        Plots a point using the transformation.
+        """
         plt.plot(xyz[0], xyz[1], self.Name, transform = self.PrinterCoordTranslation)
     
     def getJointLocation(self, xyz, plot = False):
-        """Takes a tuple (x,y,z)"""
+        """
+        Args: Takes a tuple (x,y,z) of the location of the nozzle
+              plot = True plots the mid joint
+        Return: returns a tuple of the intermediate joint position (x, y)
+        """
         x,y,z = xyz
         
         DistanceVector = (x - self.PrinterOffset[0], y - self.PrinterOffset[1])
@@ -103,7 +124,18 @@ class printer:
         
         return location
     
+    
+    
+    """
+    Polygon Creationg and plotting Functions
+    """
     def getPolygonCoords(self, xyzI, xyzF):
+        """
+        Args: Initial XYZ position tuple: (x, y, z),
+              Final XYZ position tuple: (x, y, z)
+              
+        Returns: a list of the coordinates needed from the polygon.
+        """
         
         JointInitial = self.getJointLocation(xyzI)
         JointFinal = self.getJointLocation(xyzF)
@@ -120,7 +152,7 @@ class printer:
         transformed_points = tf.transform(points_array)
         
         transformed_points = np.insert(transformed_points, 0, BasePos, axis = 0)
-        transformed_points = np.insert(transformed_points, 5, BasePos, axis = 0)
+        # transformed_points = np.insert(transformed_points, 5, BasePos, axis = 0)
         
         rot = transforms.Affine2D().rotate_deg_around(self.OriginLocation[0], self.OriginLocation[1], self.OriginLocation[2])
         
@@ -129,96 +161,204 @@ class printer:
         print(rotated_points)
         print(polyTup)
         return rotated_points
-        # return polyTup
-    
-    
-    # def returnTransformation(self):
-    #     return self.PrinterCoordTranslation
+        
+    def generate_all_possible_polygons(self, points):
+        """
+        *****INTERNAL FUNCTION*****
+        Args: points = ((x1, y1), (x2, y2), (x3, y3), ... (xn, yn))
+        returns: The union of all possible triangles from the given points and combine them into a single polygon.
+        """
+        triangles = []
+        
+        # Generate all combinations of exactly 3 points (triangles)
+        for subset in combinations(points, 3):
+            try:
+                triangle = Polygon(subset)
+                # Only consider valid triangles that are not self-intersecting
+                if triangle.is_valid:
+                    triangles.append(triangle)
+            except Exception as e:
+                print(f"Error creating triangle from subset {subset}: {e}")
+        
+        # Combine all valid triangles into a single union
+        combined_polygon = unary_union(triangles)
+        
+        return combined_polygon
     
     def getScaledPolygon(self, xyzI, xyzF):
+        """
+        Args: Initial XYZ position tuple: (x, y, z),
+              Final XYZ position tuple: (x, y, z)
+              
+        Returns: Scaled up polygon from the getPolygon to apply a border condition to the function
+        """
         
-        p = Polygon(self.getPolygonCoords(xyzI, xyzF))
-        
-        # s_fact = 1 + self.scaleFactor
-        # base = (int(self.baseLocation[0].get_xdata()[0]), int(self.baseLocation[0].get_ydata()[0]))
-        # scaled = scale(p, xfact = s_fact, yfact = s_fact, origin = base)
-        
+        p = self.getPolygon(xyzI, xyzF)
         scaled = p.buffer(self.scaleFactor)
         
         return scaled
     
     def getPolygon(self, xyzI, xyzF):
+        """
+        *****INTERNAL FUNCTION*****
+        
+        Args: Initial XYZ position tuple: (x, y, z),
+              Final XYZ position tuple: (x, y, z)
+              
+        Returns: a polygon representing the area between the 5 points
+        """
         
         p = Polygon(self.getPolygonCoords(xyzI, xyzF))
         
+        if (not p.is_simple):
+            p = self.generate_all_possible_polygons((self.getPolygonCoords(xyzI, xyzF)))
+        
+        
         return p
     
-        
-        
-        
-        
-# class envelope:
-#     def __init__(self, printers):
-#         self.fig, self.ax = plt.subplots()
-        
-#         for p in printers:
-#             self.printers.append(p)
-            
-#             rect = patches.Rectangle(p.getOrigin(), p.getWidth(), p.getHeight(), linewidth=2, edgecolor='r', facecolor='none')
-#             self.ax.add_patch(rect)
-#             #self.
-#         plt.show()
+    def plot(self):
+        pass
 
+    
+    """
+    Printer Network Functions
+    """
+    def getTargetPosition(self):
+        """
+        Implement eventually. This one is for getting the end position from the printer
+        """
+        return(0,0,0)
+    
+    def getCurrentPosition(self):
+        """
+        Implement eventually. This one is for getting the current position from the printer
+        """
+        return(0,0,0)
+    
+    def request_status(self):
+    	base_request = ("http://{0}:{1}/rr_status?type=0").format(self.IP,"")
+    	return requests.get(base_request).json()
+    
+    def issue_gcode(self, com, filename=""):
+    	base_request = ("http://{0}:{1}/rr_gcode?gcode=" + self.gcode_list[com] + self.filename).format(self.IP,"")
+    	r = requests.get(base_request)
+    	return r
+        
+        
+        
+class envelope:
+    def __init__(self, printers):
+        self.fig, self.ax = plt.subplots()
+        self.printerList = printers
+        
+        minX = self.getMinMaxX()[0]
+        maxX = self.getMinMaxX()[1]
+        
+        minY = self.getMinMaxY()[0]
+        maxY = self.getMinMaxY()[1]
+        
+        self.ax.set_xlim(minX, maxX)
+        self.ax.set_ylim(minY, maxY)
+        
+        plt.show()
+
+    def getMinMaxX(self):
+        pass
+    def getMinMaxY(self):
+        pass
+    
+    
 if __name__ == "__main__":
     # e = envelope([])
     
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
+    fig2, ax2 = plt.subplots()
     
-    ax.set_xlim(-500, 500)
-    ax.set_ylim(-500, 500)
+    ax2.set_xlim(-200, 1000)
+    ax2.set_ylim(-100, 1000)
     
-    p = printer((50,50,0), (150, -35), ax)
+    p = printer((0,0,0), (150, -35), "IP3", ax2)
     
     # print(p.getLocations((0,0,0)))
     
-    locationI = (0,300,0)
-    locationF = (300,300,0)
+    locationI = (300,300,0)
+    locationF = (0,300,0)
     
     p.plotPrinterPoint(locationI)
-    p.getJointLocation(locationI, True)
+    p.getJointLocation(locationI)
     
     p.plotPrinterPoint(locationF)
-    p.getJointLocation(locationF, True)
+    p.getJointLocation(locationF)
     
-    polygon1 = p.getPolygon(locationI, locationF)
+    # polygon1 = p.getPolygon(locationI, locationF)
     
     polygon2 = p.getScaledPolygon(locationI, locationF)
     
-    
-    
-    
-    
-    x1, y1 = polygon1.exterior.xy
+    # x1, y1 = polygon1.exterior.xy
     
     x2, y2 = polygon2.exterior.xy
     
     
-    # transform = p.returnTransformation()
+    # ax.fill(x1, y1, alpha=0.5, fc='blue', label='Polygon 1')
     
-    # tfC1 = transform.transform(list(zip(x1, y1)))
-    # transformed_polygon = Polygon(tfC1)
-    
-    # ax.plot(*transformed_polygon.exterior.xy, color='red', label='Transformed')
-
+    ax2.fill(x2, y2, alpha=0.5, fc='red', label='Polygon 2')
     
     
     
-    ax.fill(x1, y1, alpha=0.5, fc='blue', label='Polygon 1')
     
-    ax.fill(x2, y2, alpha=0.5, fc='red', label='Polygon 2')
+    p2 = printer((0,600,270), (150, -35), "IP3", ax2)
     
-
-
+    # print(p.getLocations((0,0,0)))
+    
+    locationI2 = (300,300,0)
+    locationF2 = (0,300,0)
+    
+    p2.plotPrinterPoint(locationI2)
+    p2.getJointLocation(locationI2, True)
+    
+    p2.plotPrinterPoint(locationF2)
+    p2.getJointLocation(locationF2, True)
+    
+    # Poly1 = p2.getPolygon(locationI2, locationF2)
+    
+    Poly2 = p2.getScaledPolygon(locationI2, locationF2)
+    
+    # x3, y3 = Poly1.exterior.xy
+    
+    x4, y4 = Poly2.exterior.xy
+    
+    
+    # ax.fill(x3, y3, alpha=0.5, fc='blue', label='P1 1')
+    
+    ax2.fill(x4, y4, alpha=0.5, fc='red', label='P2 2')
+    
+    
+    
+    p3 = printer((300,900,180), (150, -35), "IP3", ax2)
+    
+    # print(p.getLocations((0,0,0)))
+    
+    locationI2 = (300,300,0)
+    locationF2 = (0,300,0)
+    
+    p3.plotPrinterPoint(locationI2)
+    p3.getJointLocation(locationI2, True)
+    
+    p3.plotPrinterPoint(locationF2)
+    p3.getJointLocation(locationF2, True)
+    
+    # Poly1 = p2.getPolygon(locationI2, locationF2)
+    
+    Poly3 = p3.getScaledPolygon(locationI2, locationF2)
+    
+    # x3, y3 = Poly1.exterior.xy
+    
+    x5, y5 = Poly3.exterior.xy
+    
+    
+    # ax.fill(x3, y3, alpha=0.5, fc='blue', label='P1 1')
+    
+    ax2.fill(x5, y5, alpha=0.5, fc='red', label='P2 2')
 
     plt.show()
 
